be43a1661501637dedcd6df67fb9e4218f44d04e
ZkDragonZk <75184968+ZkDragonZk@users.noreply.github.com>
2021-10-23 19:37:44 -0300

    Add files via upload

---
+++ wiki_jogos/src/js/index.js
@@ -0,0 +1,6 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+  searchForm.classList.toggle('active');
+  navbar.classList.remove('active');
+}
